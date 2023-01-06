3cb1ca2219138f9116144b013b583ebb780aab23
Anil Kumar Behera <ark845612@gmail.com>
2023-01-07 00:56:31 +0530

    some commits

--- src/components/welco.jsx
+++ src/components/welco.jsx
@@ -2,7 +2,15 @@ const Welco = (props) => {
     const name=props.name;
     return ( 
         <div className="welcome panel">
-            <p>Welcome User! {name}</p>
+            <button className="btn btn-danger" style={{
+                width: "80px",
+                float: "right"
+            }}>Sign Out</button>
+            <p style={{
+                clear:"right"
+            }}>Welcome User! {name}</p>
+            {/* <div className="col-1"> */}
+            {/* </div> */}
         </div>
      );
 }
