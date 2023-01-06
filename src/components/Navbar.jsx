b4ca5bf1a49b148ac2328846c0c15caefff1e33e
Anil Kumar Behera <ark845612@gmail.com>
2023-01-07 01:01:49 +0530

    welco

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { getAuth, onAuthStateChanged} from "firebase/auth";
 import { db } from "./firebase";
 import { doc, getDoc } from "firebase/firestore";
-import Welco from "./Welcome";
+import Welco from "./Welco";
 
 const Navbar = () => {
   const [isLogin, setLogin] = useState(false);
