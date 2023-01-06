9d9b4848e6e466b23f55f434f5e1e63e9fe87020
Anil Kumar Behera <ark845612@gmail.com>
2023-01-06 21:31:41 +0530

    added few files, added front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+// import firebase from './components/firebase';
+import SignIn from './components/auth/signIn';
+import SignUp from './components/auth/signUp';
+import AuthDetails from './components/auth/authDetails';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return(
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <div className="content">
+            <Switch>
+              <Route exact path="/login">
+                <SignIn />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/signUp">
+                <SignUp />
+              </Route>
+            </Switch>
+          </div>
+          {/* <SignIn />
+          <SignUp />
+          <AuthDetails /> */}
+        </div>
+      </Router>
+    );
+  }
+  
 
 export default App;
