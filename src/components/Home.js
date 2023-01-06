eeec4bf30f51a5d78010808087f044726eea625d
Anil Kumar Behera <ark845612@gmail.com>
2023-01-06 21:31:41 +0530

    added few files, added front

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return ( 
+        <div className="buddhi" >
+            <img src="https://i.postimg.cc/7hZrwc7q/pexels-korhan-erdol-2344620.jpg" alt=""  id="buddhi"/>
+            <h1 className="special">Some Text</h1>
+            <p style={{
+                textAlign: "left"
+            }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, quidem neque repellendus laborum atque omnis nisi quo quaerat ipsam voluptas ad soluta fugit odit repellat. Cumque ea dolore nesciunt quam quae aut veritatis nobis nisi ullam quia amet voluptatem perferendis quis minima ex aperiam, saepe consequuntur numquam assumenda, totam voluptatibus.</p>
+        </div>
+     );
+}
+ 
+export default Home;
