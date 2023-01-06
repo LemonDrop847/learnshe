40935391d5c5970243e89c2f7c91e9dfa151c244
LemonDrop847 <66952539+LemonDrop847@users.noreply.github.com>
2023-01-07 00:49:51 +0530

    Unused Import
    Changes to be committed:
    	modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Extra from "./extra";
 import { useState } from "react";
-import { getAuth, onAuthStateChanged , signOut } from "firebase/auth";
+import { getAuth, onAuthStateChanged} from "firebase/auth";
 import { db } from "./firebase";
 import { doc, getDoc } from "firebase/firestore";
 import Welco from "./Welcome";
