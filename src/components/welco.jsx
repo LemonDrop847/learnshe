f136d95bdd83e1a0fdec121c69ec548d338e2bfb
Anil Kumar Behera <ark845612@gmail.com>
2023-01-07 01:01:13 +0530

    some changes

--- src/components/welco.jsx
+++ src/components/welco.jsx
@@ -1,17 +1,27 @@
+import { getAuth,signOut } from "firebase/auth";
+
 const Welco = (props) => {
     const name=props.name;
+    const auth = getAuth();
+    const userSignOut = () => {
+        signOut(auth)
+          .then(() => {
+            console.log("sign out successful");
+          })
+          .catch((error) => console.log(error));
+      };
     return ( 
         <div className="welcome panel">
-            <button className="btn btn-danger" style={{
-                width: "80px",
-                float: "right"
-            }}>Sign Out</button>
-            <p style={{
-                clear:"right"
-            }}>Welcome User! {name}</p>
-            {/* <div className="col-1"> */}
-            {/* </div> */}
+            <p>Welcome User! {name}</p>
+            <div className="col-1">
+        <button className="btn btn-danger" style={{
+            margin: "50px 50px 50px",
+            width: "80px"
+         }} onClick={userSignOut}>Sign Out
+        </button>
+    </div>
         </div>
+        
      );
 }
  
