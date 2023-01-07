15465b2410c6d762eaf978783548bc5b1c1ea2eb
LemonDrop847 <66952539+LemonDrop847@users.noreply.github.com>
2023-01-07 09:00:00 +0530

    Reload
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/Welcome.jsx
    	new file:   src/components/data/skills.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,6 +7,7 @@ const Welcome = (props) => {
         signOut(auth)
           .then(() => {
             console.log("sign out successful");
+            window.location.reload(true);
           })
           .catch((error) => console.log(error));
       };
