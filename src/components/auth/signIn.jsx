5ca161ff5f235c1729a72dacb3ece9b6b34f41fb
Anil Kumar Behera <ark845612@gmail.com>
2023-01-07 16:04:12 +0530

    sign up sign in style added

--- src/components/auth/signIn.jsx
+++ src/components/auth/signIn.jsx
@@ -20,19 +20,23 @@ const SignIn = () => {
   return (
     <div className="sign-in-container">
       <form onSubmit={signIn}>
-        <h1>Log In to your Account</h1>
+        <h2>Log In To Your Account</h2>
+        <label>Email</label>
         <input
           type="email"
           placeholder="Enter your email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         ></input>
+        <br />
+        <label >Password</label>
         <input
           type="password"
           placeholder="Enter your password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         ></input>
+        <br />
         <button type="submit">Log In</button>
       </form>
     </div>
