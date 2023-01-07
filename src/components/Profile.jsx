2ad713a15f7dcad628c583673ef1e9442ecd2a67
LemonDrop847 <nitinmishra847@gmail.com>
2023-01-07 18:16:31 +0530

    Vars Added
    Changes to be committed:
    	modified:   src/components/Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,35 @@
 import { Link } from "react-router-dom";
-
-const Profile = () => {
+import {db} from './firebase';
+import { useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+const Profile = async () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const [userName, setName] = useState("");
+    const [skills, setSkills] = useState("");
+    const [email, setEmail] = useState("");
+    const [otherstuff1, setStuff] = useState("");
+    const [otherstuff2, setStuff2] = useState("");
+    if(user)
+    {
+        const docRef = doc(db, "users", user.email);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            setName( docSnap.data().name);
+            setEmail( docSnap.data().email);
+            setSkills( docSnap.data().skillstring);
+          } else {
+            console.log("No data!");
+          }
+    }
+    else
+    {
+        console.log("No username!");
+    }
     return ( 
         <div className="profile">
-            <div className="special">MyProfile</div>
+            <div className="special">MyProfile Name: </div>
         </div>
      );
 }
