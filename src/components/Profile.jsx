b402f1a6a279ecbfd07f19a839dd6548957f4b92
LemonDrop847 <66952539+LemonDrop847@users.noreply.github.com>
2023-01-07 18:27:52 +0530

    Fix
    Changes to be committed:
    	modified:   src/components/Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,36 @@
 import { Link } from "react-router-dom";
-import {db} from './firebase';
+import { db } from "./firebase";
 import { useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-const Profile = async () => {
-    const auth = getAuth();
-    const user = auth.currentUser;
-    const docRef = doc(db, "users", user.email);
-    const docSnap = await getDoc(docRef);
-    const name= docSnap.data().name
-    const skills= docSnap.data().email;
-    const email  = docSnap.data().skillstring;
-   
-    return ( 
-        <div className="profile">
-            <div className="special">MyProfile Name: {name} email: {email} skills: {skills} </div>
-        </div>
-     );
-}
- 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const Profile = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [skills, setSkill] = useState("");
+  const auth = getAuth();
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      const docRef = doc(db, "users", user.email);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setName(docSnap.data().name);
+        setEmail(docSnap.data().email);
+        setSkill(docSnap.data().skillstring);
+      } else {
+        console.log("No data!");
+      }
+    } else {
+      setName(null);
+    }
+  });
+
+  return (
+    <div className="profile">
+      <div className="special">
+        MyProfile Name: {name} email: {email} skills: {skills}
+      </div>
+    </div>
+  );
+};
+
 export default Profile;
