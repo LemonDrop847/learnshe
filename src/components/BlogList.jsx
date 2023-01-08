ccd4c389b567f95845bd46035143d430707bee4d
Anil Kumar Behera <ark845612@gmail.com>
2023-01-08 15:45:24 +0530

    some changes

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,5 +1,5 @@
 // import { Link } from "react-router-dom";
-import Iframe from 'react-iframe';
+// import Iframe from 'react-iframe';
 const BlogList = (props) => {
     const blogs =props.blogs;
     const title=props.title;
